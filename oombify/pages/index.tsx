faae9c99c5d81ecb8ed4d849cd6647e25c49410e
abhinavgkrishnan <abhinavgkrishnan@gmail.com>
2022-03-08 12:24:15 +0530

    starting

---
+++ oombify/pages/index.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div className="">
+    <h1>lol</h1>
+    <main>
+      {/*sidebar*/}
+      {/* Center */}
+    </main>
+
+    <div>{/* player */}</div>
+    </div>
+  );
+}
